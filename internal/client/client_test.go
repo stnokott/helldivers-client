644f0b658fd4f40b72a5ceb39543b3847110e9d4
stnokott <30934296+stnokott@users.noreply.github.com>
2024-03-27 16:22:34 +0100

    fix: ineffectual assignment

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -61,10 +61,10 @@ func TestClientHosts(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			client, err := New(tt.host)
+			client, _ := New(tt.host)
 			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer cancel()
-			_, err = client.Seasons(ctx)
+			_, err := client.Seasons(ctx)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Client.Seasons() error = %v, wantErr %v", err, tt.wantErr)
 				return
