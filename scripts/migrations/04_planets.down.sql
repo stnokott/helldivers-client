4b82602885a0f5599d728d9103c4126f9ae5e306
stnokott <30934296+stnokott@users.noreply.github.com>
2024-04-27 16:35:49 +0200

    fix: add IF EXISTS clause to trigger and function DROPs

--- scripts/migrations/04_planets.down.sql
+++ scripts/migrations/04_planets.down.sql
@@ -1,8 +1,14 @@
-DROP TRIGGER validate_planet_hazard_refs ON planets;
-DROP FUNCTION validate_planet_hazard_refs;
+DROP TRIGGER IF EXISTS validate_planet_hazard_refs ON planets;
+
+
+DROP FUNCTION IF EXISTS validate_planet_hazard_refs;
+
 
 DROP TABLE IF EXISTS planets;
 
+
 DROP TABLE IF EXISTS biomes;
 
+
 DROP TABLE IF EXISTS hazards;
+
--- scripts/migrations/07_snapshots.down.sql
+++ scripts/migrations/07_snapshots.down.sql
@@ -1,17 +1,29 @@
-DROP TRIGGER validate_planet_snapshot_refs ON planet_snapshots;
-DROP FUNCTION validate_planet_snapshot_refs;
+DROP TRIGGER IF EXISTS validate_planet_snapshot_refs ON planet_snapshots;
 
-DROP TRIGGER validate_snapshot_refs ON snapshots;
-DROP FUNCTION validate_snapshot_refs;
+
+DROP FUNCTION IF EXISTS validate_planet_snapshot_refs;
+
+
+DROP TRIGGER IF EXISTS validate_snapshot_refs ON snapshots;
+
+
+DROP FUNCTION IF EXISTS validate_snapshot_refs;
+
 
 DROP TABLE IF EXISTS snapshots;
 
+
 DROP TABLE IF EXISTS planet_snapshots;
+
 
 DROP TABLE IF EXISTS event_snapshots;
 
+
 DROP TABLE IF EXISTS assignment_snapshots;
+
 
 DROP TABLE IF EXISTS war_snapshots;
 
+
 DROP TABLE IF EXISTS snapshot_statistics;
+
