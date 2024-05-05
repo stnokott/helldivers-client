984ae0645e018bb269a15c73ba7c829e32b33fb7
NoahK <30934296+stnokott@users.noreply.github.com>
2024-05-05 16:41:40 +0200

    fix(dev): remove unused function (#31)

--- internal/transform/transform.go
+++ internal/transform/transform.go
@@ -2,10 +2,6 @@
 package transform
 
 import (
-	"fmt"
-	"reflect"
-	"strings"
-
 	"github.com/stnokott/helldivers-client/internal/api"
 )
 
@@ -17,19 +13,4 @@ type APIData struct {
 	Campaigns   *[]api.Campaign2
 	Dispatches  *[]api.Dispatch
 	Assignments *[]api.Assignment2
-}
-
-// errFromNils returns an error containing the list of nil fields in v.
-func errFromNils[T any](v *T) error {
-	var names []string
-	value := reflect.ValueOf(v).Elem()
-	valueType := value.Type()
-	for i := 0; i < value.NumField(); i++ {
-		field := value.Field(i)
-		if field.IsNil() {
-			names = append(names, valueType.Field(i).Name)
-		}
-	}
-
-	return fmt.Errorf("nil fields in %s struct: %s", valueType.Name(), strings.Join(names, ", "))
 }
